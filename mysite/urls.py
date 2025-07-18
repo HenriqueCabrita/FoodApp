9c719f9dcf299103250209554a3d9d3659d6852a
kulhas <hmrc_aim@hotmail.com>
2025-07-18 14:27:41 +0100

    deployment

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin 
-from django.urls import path, include 
+from django.contrib import admin
+from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as authentication_views
 from django.conf import settings
@@ -27,7 +27,6 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', authentication_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', authentication_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('profile/', user_views.profilepage, name='profile'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Ensure to include MEDIA_URL here
 
 
